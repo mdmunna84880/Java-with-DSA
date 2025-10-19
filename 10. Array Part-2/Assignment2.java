daf3b846bb14ef9a37c3bfda5a7471bccf03c57c
mdmunna84880 <mdmunna19434@gmail.com>
2025-10-19 07:59:50 +0530

    Revise: Array Part-2 Lession of Apna College

--- 10. Array Part-2/Assignment2.java
+++ 10. Array Part-2/Assignment2.java
@@ -1,13 +1,17 @@
 public class Assignment2 {
     // ? First Method
     public static int search(int num[], int target){
+
         int low = 0;
         int high = num.length - 1;
+
         while(low <= high){
+
             int mid = (low + high)/2;
+
             if(num[mid] == target){
                 return mid;
-            }else if(num[mid] > num[low]){
+            }else if(num[mid] >= num[low]){
                 if(num[mid] > target && num[low] <= target){
                     high = mid-1;
                 }else{
@@ -23,11 +27,12 @@ public class Assignment2 {
         }
         return -1;
     }
-    // ? Second Method
 
     public static void main(String[] args) {
+
         int num[] = {4, 5, 6, 7, 0, 1, 2};
         int target = 3;
+
         System.out.println("The element index found on "+search(num, target));
     }
 }
--- 10. Array Part-2/Assignment3.java
+++ 10. Array Part-2/Assignment3.java
@@ -13,7 +13,7 @@ public class Assignment3 {
         return maxProfit;
     }
     public static void main(String[] args) {
-        int prices[] = {7, 6, 4, 3, 1};
+        int prices[] = {7, 1, 5, 3, 6, 4};
         System.out.println(maxProfit(prices));
     }
 }
